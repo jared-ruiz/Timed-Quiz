3e5f899ddc44010903d416a31bd725d6b9041d33
Jared Ruiz <jared707@gmail.com>
2022-02-26 21:05:24 -0600

    Set up question buttons and some styling.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// console.dir(document);
+
+//id select the start button
+var startSelect = document.querySelector("#start-btn");
+
+var quizStart = function() {
+
+}
+
+//execute quizStart upon click
+startSelect.addEventListener("click", quizStart);
